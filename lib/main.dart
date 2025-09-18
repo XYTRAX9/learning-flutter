5fa126a33f2766f80c98b68681ca941c561aa267
XYTRAX9 <yaroslav.dzhum@gmail.com>
2025-09-18 21:14:21 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Learning());
+}
+
+class Learning extends StatelessWidget {
+  const Learning({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        textTheme: TextTheme(
+          bodyMedium: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
+          labelSmall: TextStyle(
+            color: Colors.white.withOpacity(0.6),
+            fontWeight: FontWeight.w700,
+            fontSize: 14,
+          ),
+        )
+      ),
+      home: const MyHomePage(title: 'Tutorial App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, i) => ListTile(
+            title: Text(
+                "Bitcoin",
+                style: theme.textTheme.bodyMedium,
+            ),
+            subtitle: Text(
+                "200000\$",
+                style: theme.textTheme.labelSmall,
+            ),
+          ),
+      ),
+    );
+  }
+}
